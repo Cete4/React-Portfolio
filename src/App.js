e9512e8d043739ded5a7b2155640a8723acb91e8
Cete4 <46322113+Cete4@users.noreply.github.com>
2022-04-19 16:08:24 -0500

    Starting on the font change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-
+import React from "react";
+import './App.css'
+import {Col, Container, Navbar, Row} from "react-bootstrap";
+import anime from 'animejs';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+
+    const animationRef = React.useRef(null);
+    React.useEffect(() => {
+        animationRef.current = anime({
+            targets: ".el",
+            translateX: 250,
+            delay: function (el, i) {
+                return i * 100;
+            },
+            loop: true,
+            direction: "alternate",
+            easing: "easeInOutSine"
+        });
+    }, []);
+
+    return (
+        <div className="App">
+            <Row className="outershell">
+                <Container fluid>
+                    <Row>
+                        <Col xs>
+
+                        </Col>
+                        <Col xs={10}>
+                            <p>Micah BigEagle</p>
+                            <hr></hr>
+                        </Col>
+                        <Col>
+
+                        </Col>
+                    </Row>
+                </Container>
+            </Row>
+        </div>
+
+    );
 }
 
 export default App;
