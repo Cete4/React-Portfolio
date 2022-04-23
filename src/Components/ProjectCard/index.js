7bc859e2e2371675797b83ef69501b8831aedeed
Cete4 <46322113+Cete4@users.noreply.github.com>
2022-04-23 13:50:22 -0500

    Refactoring and aligning the cards to display them properly.

--- src/Components/ProjectCard/index.js
+++ src/Components/ProjectCard/index.js
@@ -2,11 +2,10 @@ import React from "react";
 import './style.css'
 import {Card} from "react-bootstrap";
 
-
 function ProjectCard(props) {
     return (
         <Card className="ProjectCard">
-            <Card.Img src="" alt={props.cardTitle}></Card.Img>
+            {/*<Card.Img src={props.cardImage} alt={props.cardTitle}></Card.Img>*/}
             <Card.Title>{props.cardTitle}</Card.Title>
             <Card.Body>{props.cardBody}</Card.Body>
             <a className="CardAnchor" href={props.cardLink} target="_blank">Check It Out</a>
