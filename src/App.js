5615105b15efec81b07145808cb89c27550a1d7f
Cete4 <46322113+Cete4@users.noreply.github.com>
2023-08-17 19:26:24 -0500

    Removed Google Analytics and routing

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
-import React, {useEffect} from "react";
+import React from "react";
 import './App.css'
-import ReactGA from 'react-ga';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from "./Components/Home/index.js";
 
-const TRACKING_ID = process.env.GOOGLE_ANALYTICS_KEY; // OUR_TRACKING_ID
-
-
 function App() {
-
-    // Initialize Google Analytics
-    ReactGA.initialize(TRACKING_ID);
-
-    useEffect(() => {
-        ReactGA.pageview(window.location.pathname + window.location.search);
-    }, []);
-
-
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="React-Portfolio" element={<Home/>}/>
-                <Route path="React-Portfolio/" element={<Home/>}/>
-            </Routes>
-        </Router>
+        <Home/>
     );
 }
 
